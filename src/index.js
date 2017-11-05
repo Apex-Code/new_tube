953016411898ef10baf473450b566b28635abe38
Apex-Code <upstaterounder@gmail.com>
2017-11-05 01:01:15 -0500

    update index.js to be Class Component, update search bar

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
-
+import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyBQ8d7_ZpZQLwCUH5jCCaAinmoQLUwIt6Q';
 
-const App = () => {
-    return <div>
-        <SearchBar/>
-    </div>;
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: 'surfboards'},  (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render()
+    {
+        return(
+        <div>
+            <SearchBar/>
+        </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
