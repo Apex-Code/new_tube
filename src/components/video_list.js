05798a122d18311a79c055e279f87d23335b356d
Apex-Code <upstaterounder@gmail.com>
2017-11-06 01:32:28 -0600

    update videolist to display title.

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,10 +3,10 @@ import VideoListItem from './video_list_item.js'
 
 const VideoList = (props) => {
     const videoItems = props.videos.map((video) => {
-       return < VideoListItem video={video} />
+       return < VideoListItem key={video.etag} video={video} />
     });
     return (
-      <ul className="col-md-4 list-group">
+      <ul className="col-md-6 list-group">
           {videoItems}
       </ul>
     );
